b0d3e3c6cf325d8fe45f93c166b8c1e2eec5b556
2022102317 <2022102317@LAB1-012>
2024-03-05 20:39:51 -0300

    Adicionar arquivos de projeto.

---
+++ Aula01/Calculadora.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula01
+{
+    public class Calculadora
+    {
+        public int Soma(int n1, int n2)
+        { 
+            int resultado = n1 + n2;
+            return resultado;
+        
+
+        }
+    }
+}
---
+++ Aula01/Program.cs
@@ -0,0 +1,38 @@
+﻿
+//altere o programa para calcular o imc de uma pessoa
+// imc = peso / altura * altura
+using Aula01;
+using System.Globalization;
+
+bool entrada = true;
+do
+{
+    try
+    {
+        Console.WriteLine("Informe o n1 ");
+        int n1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Informe o n2 ");
+        int n2 = Convert.ToInt32(Console.ReadLine());
+
+        Calculadora calculo = new Calculadora();
+        Console.WriteLine(calculo.Soma(n1, n2));
+
+        //Console.WriteLine("Informe o peso atual: ");
+        //int peso = Convert.ToInt32(Console.ReadLine());
+        //Console.WriteLine("Informe a altura atual: ");
+        //float altura = float.Parse(Console.ReadLine());
+        //CalculaIMC calculaIMC = new CalculaIMC();
+        //Console.WriteLine( calculaIMC.Calcular(peso, altura));
+        //string resultado=calculaIMC.Calcular(peso, altura);
+        //Console.WriteLine(resultado);
+     
+        entrada = false;
+    }
+    catch (FormatException e)
+    {
+        Console.WriteLine("O peso deve ser um número inteiro e a altura em numero decimal");
+    }
+} while (entrada);
+
+
+
